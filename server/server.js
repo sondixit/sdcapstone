09edf7726d7517fab74b6760349818bfb0e81c56
pjananth22123 <pjananth22123@gmail.com>
2022-12-30 12:23:59 +0530

    sixth commit

--- server/server.js
+++ server/server.js
@@ -27,11 +27,7 @@ const allowedOrigins = [
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+        callback(null, true)
     },
     optionsSuccessStatus: 200
 }
